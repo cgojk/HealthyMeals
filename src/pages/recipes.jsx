43e142e1b4e43e7c7ea41f2efd4e7bdd2a3710ed
catalina <catalinaruedaf@hotmail.com>
2026-05-31 07:08:56 +1000

    starting marup recipes page

--- src/pages/recipes.jsx
+++ src/pages/recipes.jsx
@@ -1,30 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import CalltoAction from "../components/CalltoAction";
+import { FaChevronDown } from "react-icons/fa";
 
-  <link rel="icon" type="image/png" sizes="32x32" href="./assets/images/favicon-32x32.png">
-  
-  <title>Frontend Mentor | Recipe finder website</title>
-</head>
-<body>
 
-  Explore our simple, healthy recipes
+export default function recipes() {
 
-  Discover eight quick, whole-food dishes that fit real-life 
-  schedules and taste amazing. Use the search bar to find a 
-  recipe by name or ingredient, or simply scroll the list and 
-  let something delicious catch your eye.
+  const toggle = () => {
+        const dropdown = document.querySelector('.dropdown-menu');
+        dropdown.classList.toggle('show');
+        
+    }
 
-  Max Prep Time
-  Max Cook Time
+  return (
+    <>
+    <Navigation/>
+    <section className="recipes">
+    <h1 className="title__hero">Explore our simple, healthy recipes</h1>
+    <p className="hero__text">Discover eight quick, whole-food dishes that fit real-life schedules and taste amazing. Use the search bar to find a recipe by name or ingredient, or simply scroll the list and let something delicious catch your eye.</p>
+     <div className="dropdown-menu">
+                 <button className="dropdown-toggle" onClick={toggle}>
+                     Max Prep <FaChevronDown />
+                 </button>
+                 <ul>
+                     <li><a href="#" onClick={toggle}>0 Minutes</a></li>
+                     <li><a href="#" onClick={toggle}>5 Minutes</a></li>
+                     <li><a href="#" onClick={toggle}>10 Minutes</a></li>
+                     <li><a href="#" onClick={toggle}>Clear</a></li>
+                 </ul>
+             </div>
 
-  Search by name or ingredient...
-  
-  <!-- Populate recipes using data from the data.json file -->
+     <div className="dropdown__menu--cookingTime">
+      <div className="dropdown-menu">
+                  <button className="dropdown-toggle" onClick={toggle}>
+                      Cooking time <FaChevronDown />
+                  </button>
+                  <ul>
+                      <li><a href="#" onClick={toggle}>0 Minutes</a></li>
+                      <li><a href="#" onClick={toggle}>5 Minutes</a></li>
+                      <li><a href="#" onClick={toggle}>10 Minutes</a></li>
+                      <li><a href="#" onClick={toggle}>15 Minutes</a></li>
+                        <li><a href="#" onClick={toggle}>20 Minutes</a></li>
+                     
+                      <li><a href="#" onClick={toggle}>Clear</a></li>
+                  </ul>
+              </div>
+      
+      </div>
+      <div className="search__button">
+       
+        <input type="text" placeholder="Search by name or ingredient" className="search__input" />
+        <button className="btn">Search</button>
+      </div>
 
-  Made with ❤️ and 🥑
+      
+    </section>
+    <CalltoAction/>
+    <Footer/>
 
-</body>
-</html>
+    </>
+  )
+    }
