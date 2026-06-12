e1ef865c2604bece69d14fb05054d3cb0b1172f8
catalina <catalinaruedaf@hotmail.com>
2026-06-12 13:19:56 +1000

    orginisng styling the navigaiton

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -22,27 +22,14 @@ return (
 
 <header className="header max-container">
 <div className="header__inner">
+  
         <Link to="/home" className="nav-logo-link">
         <img src={logo} className=" nav-logo" alt=" Healthy meals" />
        </Link>
 
-
-     {/* hamburguer button only visible on mobile */}
-     
-
-     <button
-        className="menu-btn"
-        aria-label="toggle menu"
-        onClick={() => setMenuOpen(!menuOpen)}
-    >
-      {menuOpen ? <FaTimes /> : <img src={hamburguerMenu} alt="Open menu" /> }
-    </button>
-
-    {/* Nav list, toggled open on mobile */}
-
-  <nav className="navigation" aria-label="homepage Navigation">
-        <div className="container__items--navigation">
-        <ul className={`nav__list ${menuOpen ? "open" : ""}`}>
+  <nav className={`navigation  ${menuOpen ? "open" : ""}`}aria-label="homepage Navigation">
+      
+        <ul className="nav__list ">
            
             <li className="nav__items">
 
@@ -60,13 +47,21 @@ return (
                 onClick={handleLinkClick}
                 className="nav__items__links" >Recipes</NavLink>
             </li>
-              <ButtonRecipes className="button-recipes">Browse recipes</ButtonRecipes> 
-        </ul>
+             
 
             
+        </ul>
+         <ButtonRecipes className="button-recipes">Browse recipes</ButtonRecipes> 
        
-        </div>
      </nav>
+
+        <button
+        className="menu-btn"
+        aria-label="toggle menu"
+        onClick={() => setMenuOpen(!menuOpen)}
+    >
+      {menuOpen ? <FaTimes /> : <img src={hamburguerMenu} alt="Open menu" /> }
+    </button>
     </div>
      
  </header>
