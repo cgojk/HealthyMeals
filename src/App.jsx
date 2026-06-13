856af722ccc1a298a37bab37e4fe3e694de263e7
catalina <catalinaruedaf@hotmail.com>
2026-06-14 09:35:53 +1000

    change updating

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
-// import Footer from './pages/Footer';
+
 
 import Home from "./pages/home";
 import About from "./pages/about";
 import Recipes from "./pages/recipes";
-import RecipesDetails from "./pages/recipeDetails";
+import RecipesDetails from "./pages/RecipeDetails";
 
 import Layout from "./components/Layout";
 
@@ -17,18 +17,16 @@ import Layout from "./components/Layout";
 export default function App() {
   return (
 
-    <BrowserRouter>
-    
-      <Routes>
-        <Route element={<Layout />}>
-  <Route path="/" element={<Home />} />
-  <Route path="/home" element={<Home />} />
-  <Route path="/about" element={<About />} />
-  <Route path="/recipes" element={<Recipes />} />
-  <Route path="/recipes/:slug" element={<RecipesDetails />} />
-</Route>
-       
-      </Routes>
-    </BrowserRouter>
+<BrowserRouter>
+    <Routes>
+      <Route element={<Layout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/recipes" element={<Recipes />} />
+      <Route path="/recipes/:slug" element={<RecipesDetails />} />
+  </Route>
+  </Routes>
+</BrowserRouter>
   );
 }
